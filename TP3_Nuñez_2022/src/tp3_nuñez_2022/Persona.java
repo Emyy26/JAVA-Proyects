5df3d30b8efea47ab76d7cd6348738a867192150
Emyy26 <emyy26@gmail.com>
2022-09-14 16:07:59 -0300

    tp3 notebook

---
+++ TP3_Nuñez_2022/src/tp3_nuñez_2022/Persona.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_nuñez_2022;
+
+/**
+ *
+ * @author emi
+ */
+public class Persona {
+
+    private String nombre;
+    private String sexo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+}
+
---
+++ TP3_Nuñez_2022/src/tp3_nuñez_2022/TP3_Nuñez_2022.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3_nuñez_2022;
+
+/**
+ *
+ * @author emi
+ */
+public class TP3_Nuñez_2022 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       principalFrame f = new principalFrame();
+       f.setVisible(true);
+    }
+    
+}
