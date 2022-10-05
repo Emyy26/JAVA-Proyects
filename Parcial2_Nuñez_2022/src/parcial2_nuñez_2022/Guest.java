7084dc0e4557587f276e7bb85132e36775fb85fd
emi <emi@emi-MS-7A62>
2022-10-04 22:49:24 -0300

    parcial 2 ejercicio 1, filtrar y agregar funciona

---
+++ Parcial2_Nuñez_2022/src/parcial2_nuñez_2022/Guest.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package parcial2_nuñez_2022;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author emi
+ */
+public class Guest extends Person{
+    private String businessName;
+    private int age;
+
+    public String getBusinessName() {
+        return businessName;
+    }
+
+    public void setBusinessName(String businessName) {
+        this.businessName = businessName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    }
---
+++ Parcial2_Nuñez_2022/src/parcial2_nuñez_2022/Staff.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package parcial2_nuñez_2022;
+
+/**
+ *
+ * @author emi
+ */
+public class Staff extends Person{
+    
+   
+    
+}
