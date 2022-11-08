3bafaadd17e9ab0a5efa881a753c4aaae8b26912
emi <emi@emi-MS-7A62>
2022-11-08 12:13:00 -0300

    Empece tp12

---
+++ TP12_Nuñez_2022/src/tp12_nuñez_2022/TP12_Nuñez_2022.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp12_nuñez_2022;
+
+/**
+ *
+ * @author emi
+ */
+public class TP12_Nuñez_2022 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
