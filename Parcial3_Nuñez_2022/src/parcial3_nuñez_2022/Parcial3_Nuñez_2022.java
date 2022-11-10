a5225850828a59a4b9095f2d84a81967485a1cda
Emyy26 <emyy26@gmail.com>
2022-11-10 15:51:50 -0300

    Parcial 3 empezado notebook

---
+++ Parcial3_Nuñez_2022/src/parcial3_nuñez_2022/Parcial3_Nuñez_2022.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package parcial3_nuñez_2022;
+
+/**
+ *
+ * @author emi
+ */
+public class Parcial3_Nuñez_2022 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Frame f = new Frame();
+       f.setVisible(true);
+    }
+    
+}
